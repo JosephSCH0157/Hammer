f42fe515d433eed56a404efbd70084dbb474789c
JosephSCH0157 <Jschramer@gmail.com>
2025-12-29 08:17:18 -0500

    Create App component with initial layout

---
+++ src/ui/pages/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <div style={{ padding: 16, fontFamily: "system-ui, sans-serif" }}>
+      <h1>Hammer v0.01</h1>
+      <p>Boot OK. Next: LocalProvider + ProjectDoc.</p>
+    </div>
+  );
+}
